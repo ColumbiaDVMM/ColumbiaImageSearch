44e9f0d60414e5d0770e8df1aee4f05160dc4e95
Svebor Karaman <svebor.karaman@gmail.com>
2015-11-10 16:10:02 +0000

    test similar v0.2

--- testSimilarSearch.py
+++ testSimilarSearch.py
@@ -12,8 +12,9 @@ import hashlib
 import datetime
 import pickle
 import urllib
-
 import json
+import numpy as np
+
 ist_down=False
 feature_num=4096
 
@@ -27,9 +28,11 @@ localuser=global_var['local_db_user']
 localpwd=global_var['local_db_pwd']
 localdb=global_var['local_db_dbname']
 
+prefix='./'
+
 def get_all_precomp_feats(feats_id):
-    precomp_featurename = 'precomp-features'
-    precomp_featurefilename = 'precomp-feats.dat'
+    precomp_featurename = 'temp_precomp-features'
+    precomp_featurefilename = 'temp_precomp-feats.dat'
     now=datetime.datetime.now()
     # fill features indices
     f_pre = open(precomp_featurename,'wb')
@@ -37,15 +40,15 @@ def get_all_precomp_feats(feats_id):
         f_pre.write(struct.pack('i',feat_id))
     f_pre.close()
     # Execute get_precomp_feats
-    command = '../get_precomp_feats '+precomp_featurename+' '+precomp_featurefilename
+    command = prefix+'get_precomp_feats '+precomp_featurename+' '+precomp_featurefilename
     #print command
     os.system(command)
     # Read results from precomp_featurefilename
     f_pre=open(precomp_featurefilename,'rb')
     feats=None
     for feat_id in feats_id:
-        one_feat = f_pre.read(feature_num*4)
-        if not feats:
+        one_feat = np.fromfile(f_pre,dtype=np.float32,count=feature_num)
+        if feats==None:
             feats=one_feat
         else:
             #concatenante with np
@@ -56,23 +59,27 @@ def get_all_precomp_feats(feats_id):
 def get_one_feat(start):
     db=MySQLdb.connect(host=localhost,user=localuser,passwd=localpwd,db=localdb)
     c=db.cursor()
-    query="select id,location from uniqueIds where htid>\""+start+"\" order by htid ASC LIMIT 1;"
+    #query="select id,location,htid from uniqueIds where htid>\""+str(start)+"\" order by htid ASC LIMIT 1;"
+    query="select id,location,htid from uniqueIds where id=\""+str(start)+"\";"
     print query
     c.execute(query)
     remax = c.fetchall()
     print remax
     if len(remax):
         feat_id = int(remax[0][0])
-        location = int(remax[0][1])
+        location = str(remax[0][1])
+        htid = int(remax[0][2])
     else:
         feat_id = 0
         location = None
+	htid = 0
     db.close()
-    return feat_htid
+    return feat_id,location,htid
 
 def compute_feats(images,testname,protoname):
     nb_images=len(images)
-    featurefilename="feats.dat"
+    print nb_images
+    featurefilename="tmp_feats"
     batch_size = min(64,nb_images)
     iteration = int(math.ceil(nb_images/float(batch_size)))
     print 'image_number:', nb_images, 'batch_size:', batch_size, 'iteration:', iteration
@@ -83,16 +90,15 @@ def compute_feats(images,testname,protoname):
     f = open(protoname,'w');
     f.write(proto)
     f.close()
-    command = prefix+'extract_nfeatures caffe_sentibank_train_iter_250000 '+protoname+ ' fc7,prob '+featurefilename.replace('\\','/')+','+fresh_featurefilename.replace('\\','/')+'_prob '+str(iteration)+' '+device;
+    command = prefix+'extract_nfeatures caffe_sentibank_train_iter_250000 '+protoname+ ' fc7,prob '+featurefilename.replace('\\','/')+','+featurefilename.replace('\\','/')+'_prob '+str(iteration)+' '+device;
     print command
     os.system(command)
-    print 'sentibank time: ', time.time() - t0
     # Read results from precomp_featurefilename
-    f_feat=open(featurefilename,'rb')
+    f_feat=open(featurefilename+".dat",'rb')
     feats=None
     for feat_id in range(nb_images):
-        one_feat = f_feat.read(feature_num*4)
-        if not feats:
+        one_feat = np.fromfile(f_feat,dtype=np.float32,count=feature_num)
+        if feats==None:
             feats=one_feat
         else:
             #concatenante with np
@@ -101,24 +107,26 @@ def compute_feats(images,testname,protoname):
     return feats
 
 def fill_test(testname,feats_url):
-    image_id=0
     f = open(testname,'w')
     for image_url in feats_url:
-        urllib.urlretrieve(image_url, str(image_id)+".jpg")
-        f.write(str(image_id)+".jpg"+' 0\n')
-        image_id=image_id+1
+	image_name=image_url.rsplit('/')[-1]
+        urllib.urlretrieve(image_url, image_name)
+        f.write(image_name+' 0\n')
     f.close()
 
 if __name__ == '__main__':
     feats_id=[]
     feats_url=[]
+    feats_htid=[]
     testname = 'temp_test.txt'
     protoname = 'temp_test.prototxt'
-    device = 'GPU'
-    for start_id in range(70000000,110000000,1000000):
-        onefeat,oneurl=get_one_feat(start_id)
+    device = 'CPU' #GPU does not work?
+    #for start_id in range(70000000,110000000,1000000):
+    for start_id in range(1000000,20000000,1000000):
+        onefeat,oneurl,one_htid=get_one_feat(start_id)
         feats_id.append(onefeat)
         feats_url.append(oneurl)
+	feats_htid.append(one_htid)
     # Pre-computed features
     pre_feats=get_all_precomp_feats(feats_id)
     # Recompute features
@@ -126,5 +134,7 @@ if __name__ == '__main__':
     recomp_feats=compute_feats(feats_url,testname,protoname)
     # Compare
     for one_img in range(len(feats_id)):
-        dist=np.linalg.norm(pre_feats[one_img,:],recomp_feats[one_img,:])
-        print feats_id[one_img],dist
+	#print pre_feats.shape,pre_feats[one_img,:].shape
+	#print recomp_feats.shape,recomp_feats[one_img,:].shape
+        dist=np.linalg.norm(pre_feats[one_img,:]-recomp_feats[one_img,:])
+        print feats_id[one_img],feats_htid[one_img],feats_url[one_img],dist
