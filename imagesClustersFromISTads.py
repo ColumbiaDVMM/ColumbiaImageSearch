eb8d3f4d9699a80f5155b0b918576b3a2210ce31
svebor karaman <svebor.karaman@gmail.com>
2015-11-12 15:16:48 -0500

    clusters with url

--- imagesClustersFromISTads.py
+++ imagesClustersFromISTads.py
@@ -17,6 +17,64 @@ localdb=global_var['local_db_dbname']
 
 max_edges=100
 
+sql_withurl='select i.id,i.url,i.location,ads.url,ads.id from images i left join ads on i.ads_id=ads.id where i.location in (%s) and ads.id=(%s) order by i.id;'
+sql_nourl='select i.id,i.url,i.location,ads.url,ads.id from images i left join ads on i.ads_id=ads.id where ads.id=(%s) order by i.id;'
+sql_imgid_adid='select i.id,i.url,i.location,ads.url,ads.id from images i left join ads on i.ads_id=ads.id where ads.id=(%s) and i.id=(%s);'
+
+def getAdImageUrls(adid,imgid):
+    db=MySQLdb.connect(host=isthost,user=istuser,passwd=istpwd,db=istdb)
+    c=db.cursor()
+    sqlq = sql_imgid_adid % (adid,imgid)
+    c.execute(sqlq)
+    tmpresult = c.fetchall()
+    return tmpresult[0][3],tmpresult[0][1]
+
+def getExpandedAllInfos(imagesid,imageslocation,ad_id):
+    print "Expanding from imagesid",imagesid
+    connection = happybase.Connection('10.1.94.57')
+    tab = connection.table('aaron_memex_ht-images')
+    exp_adsid=[]
+    exp_adsurl=[]
+    exp_imagesid=[]
+    exp_imagesurl=[]
+    visited_expimagesid=[]
+    #print imagesid
+    queue_images=[str(oneimg) for oneimg in imagesid]
+    print queue_images
+    while len(queue_images)>0:
+        #onerow=tab.row(str(oneid),columns=('meta:columbia_near_dups','meta:columbia_near_dups_dist','meta:columbia_near_dups_biggest_dbid'))
+        oneid=queue_images.pop()
+        #print oneid
+        onerow=tab.row(str(oneid),columns=('meta:columbia_near_dups','meta:ads_id'))
+        #print onerow
+        if 'meta:columbia_near_dups' not in onerow.keys():
+            print "Can't expand with precomputed cache. Query API here?"
+	        continue
+        if oneid not in imagesid and oneid not in visited_expimagesid:
+	        try:
+               oneexp_ad_id=onerow['meta:ads_id']
+	    except:
+		oneexp_ad_id=''
+	    if oneexp_ad_id!=ad_id:
+            	exp_imagesid.append(oneid)
+            	exp_adsid.append(oneexp_ad_id)
+                adurl,imgurl=getAdImageUrls(oneexp_ad_id,oneid)
+                exp_adsurl.append(adurl)
+                exp_imagesurl.append(imgurl)
+        visited_expimagesid.extend([oneid])
+        neighbors_list=onerow['meta:columbia_near_dups'].rsplit(',')
+	#print neighbors_list
+	if len(neighbors_list)<max_edges:
+          for expimg in neighbors_list:
+            if expimg not in imagesid and expimg not in visited_expimagesid:
+                queue_images.extend([expimg])
+    print "Done expanding, we have:"
+    print len(set(exp_adsid)),"new ads:",set(exp_adsid)
+    print len(exp_imagesid),"new images:",exp_imagesid
+    #if len(exp_imagesid)>0:
+	#time.sleep(10)
+    return exp_adsid,exp_imagesid,exp_adsurl,exp_imagesurl
+
 def getExpanded(imagesid,imageslocation,ad_id):
     print "Expanding from imagesid",imagesid
     connection = happybase.Connection('10.1.94.57')
@@ -80,8 +138,6 @@ print len(ads_id),len(set(ads_id))
 # Get all data from IST database
 db=MySQLdb.connect(host=isthost,user=istuser,passwd=istpwd,db=istdb)
 c=db.cursor()
-sql_withurl='select i.id,i.url,i.location,ads.url,ads.id from images i left join ads on i.ads_id=ads.id where i.location in (%s) and ads.id=(%s) order by i.id;'
-sql_nourl='select i.id,i.url,i.location,ads.url,ads.id from images i left join ads on i.ads_id=ads.id where ads.id=(%s) order by i.id;'
 all_imagesid=[]
 all_expanded_imagesid=[]
 all_expanded_adsid=[]
@@ -141,7 +197,7 @@ alldata_istgt['all_adsurl']=all_adsurl
 alldata_istgt['all_adsid']=all_adsid
 alldata_istgt['all_expanded_adsid']=all_expanded_adsid
 alldata_istgt['all_expanded_imagesid']=all_expanded_imagesid
-pickle.dump(alldata_istgt,open("alldata_istgtv4.pkl","wb"))
+pickle.dump(alldata_istgt,open("alldata_istgtv5.pkl","wb"))
 
 # Get similar images from HBase
 
