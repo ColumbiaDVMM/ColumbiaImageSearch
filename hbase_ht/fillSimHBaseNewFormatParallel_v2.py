b96ceb1d1f3337b121d4ed5108f1f5362f5982c0
svebor karaman <svebor.karaman@gmail.com>
2016-04-25 10:26:47 -0400

    defining sha1_tools

--- hbase_ht/fillSimHBaseNewFormatParallel_v2.py
+++ hbase_ht/fillSimHBaseNewFormatParallel_v2.py
@@ -10,6 +10,9 @@ import hashlib
 from Queue import *
 from threading import Thread
 
+sys.path.insert(0, os.path.abspath('../tools'))
+import sha1_tools
+
 nb_threads=12
 # HBase connection pool
 hbase_conn_timeout = None
@@ -81,183 +84,11 @@ def createHBaseTable(tab_name,cf):
     except Exception as inst:
         print "[createHBaseTable] Error when creating table '{}'. {}".format(tab_name,inst)
 
-def getSHA1FromMySQL(image_id):
-    res_sha1 = None
-    if image_id:
-      db=MySQLdb.connect(host=localhost,user=localuser,passwd=localpwd,db=localdb)
-      c=db.cursor()
-      sql='SELECT sha1 FROM uniqueIds WHERE htid=\"{}\"'.format(image_id) 
-      #print sql
-      c.execute(sql)
-      res=c.fetchall()
-      if res:
-        res_sha1=res[0][0]
-    return res_sha1
-
-def get_batch_SHA1_from_mysql(image_ids):
-    res_sha1 = [None]*len(image_ids)
-    if image_ids:
-        db=MySQLdb.connect(host=localhost,user=localuser,passwd=localpwd,db=localdb)
-        c=db.cursor()
-        sql='SELECT sha1,htid FROM uniqueIds WHERE htid IN (%s)'
-        #print sql
-        c.execute(sql,','.join(image_ids))
-        res=c.fetchall()
-        for row in res:
-            res_sha1[image_ids.index(str(row[1]))]=row[0]
-    return res_sha1
-
-def getSHA1FromFile(filepath):
-    sha1 = hashlib.sha1()
-    f = open(filepath, 'rb')
-    try:
-        sha1.update(f.read())
-    finally:
-        f.close()
-    os.unlink(filepath)
-    return sha1.hexdigest()
-
-def computeSHA1(cdr_id,logf=None):
-    sha1hash = None
-    # get image url
-    with pool.connection(timeout=hbase_conn_timeout) as connection:
-        tab_samples = connection.table(tab_samples_name)
-        one_row = tab_samples.row(cdr_id)
-    #print one_row
-    doc = one_row['images:images_doc']
-    jd = json.loads(doc)
-    one_url = jd['obj_stored_url']
-    if not one_url:
-        #if logf:
-        #    logf.write("Could not get URL from cdrid {}.\n".format(cdr_id))
-        #else:
-        #    print "Could not get URL from cdrid {}.".format(cdr_id)
-        pass
-    else: # download
-        localpath = dlImage(one_url,logf)
-        # compute sha1
-        if localpath:
-            sha1hash = getSHA1FromFile(localpath)
-        else:
-            if logf:
-                logf.write("Could not download image from URL {} of cdrid {}.\n".format(one_url,cdr_id))
-            else:
-                print "Could not download image from URL {} of cdrid {}.".format(one_url,cdr_id)
-    return sha1hash
-
-def getSHA1(image_id,cdr_id,logf=None):
-    #print image_id,cdr_id
-    hash_row = None
-    if image_id:
-        with pool.connection(timeout=hbase_conn_timeout) as connection:
-            tab_hash = connection.table(tab_hash_name)
-            hash_row = tab_hash.row(str(image_id))
-    sha1hash = None
-    if hash_row:
-        sha1hash = hash_row['image:hash']
-    else:
-        #print "HBase Hash row is empty. Trying to get SHA1 from MySQL."
-        # Get hash from MySQL...
-        sha1hash = getSHA1FromMySQL(image_id)
-        # or recompute from image if failed.
-        if not sha1hash and cdr_id:
-            #print "Could not get SHA1 from MYSQL. Recomputing..."
-            sha1hash = computeSHA1(cdr_id,logf)
-    if sha1hash:
-        #print "Saving SHA1 {} for image ({},{}) in HBase".format(sha1hash,cdr_id,image_id)
-        saveSHA1(image_id,cdr_id,sha1hash.upper())
-    else:
-        save_missing_sha1(image_id,cdr_id)
-        #print "Could not get/compute SHA1 for {} {}.".format(image_id,cdr_id)
-    return sha1hash
-
-def save_missing_sha1(image_id,cdr_id):
-    with pool.connection(timeout=hbase_conn_timeout) as connection:
-        tab_missing_sha1 = connection.table(tab_missing_sha1_name)
-        # TODO maybe list of info:cdr_id if already exists?
-        if not tab_missing_sha1.row(str(image_id)):
-            tab_missing_sha1.put(str(image_id), {'info:cdr_id': str(cdr_id)})
-
 def save_missing_sim(image_id):
     with pool.connection(timeout=hbase_conn_timeout) as connection:
         tab_missing_sim = connection.table(tab_missing_sim_name)
         if not tab_missing_sim.row(str(image_id)):
             tab_missing_sim.put(str(image_id), {'info:image_id': str(image_id)})
-
-def get_batch_SHA1_from_imageids(image_ids,logf=None):
-    #print image_id,cdr_id
-    if not image_ids:
-        #logf.write("[get_batch_SHA1_from_imageids] image_ids is empty!\n")
-        return None
-    str_image_ids=[str(iid) for iid in image_ids]
-    if not str_image_ids:
-        #logf.write("[get_batch_SHA1_from_imageids] str_image_ids is empty!\n")
-	return None
-    hash_rows = None
-    #if logf:
-    #    logf.write("Looking for images: {}\n".format(",".join(str_image_ids)))
-    with pool.connection(timeout=hbase_conn_timeout) as connection:
-       # if logf:
-       #     logf.write("Connection opened on port: {}\n".format(connection.port))
-        tab_hash = connection.table(tab_hash_name)
-        hash_rows = tab_hash.rows(str_image_ids)
-    sha1hash=[]
-    misssing_sha1=[]
-    stillmissing_sha1=[]
-    # check if we have all sha1 requested
-    if len(hash_rows)==len(str_image_ids):
-        # hash_rows should have kept the order of image_ids
-        for iid,sha1 in hash_rows:
-            sha1hash.append(sha1['image:hash'])
-    else:
-        # fill whatever we got up to now
-        sha1hash=[None]*len(str_image_ids) 
-        for iid,sha1 in hash_rows:
-            sha1hash[str_image_ids.index(iid)]=sha1['image:hash']
-        missing_sha1=[str_image_ids[iid] for iid in range(len(str_image_ids)) if sha1hash[iid] is None]
-        # try to get the missing sha1 form mysql...
-        if missing_sha1:
-            sha1hash_sql = get_batch_SHA1_from_mysql(missing_sha1)
-            for missid,iid in enumerate(missing_sha1):
-                if sha1hash_sql[missid] is not None:
-                    sha1hash[str_image_ids.index(iid)]=sha1hash_sql[missid]
-                else:
-                    stillmissing_sha1.append(iid)
-            # no more fallbacks at this point.
-    # save the missing sha1
-    if stillmissing_sha1: 
-        with pool.connection(timeout=hbase_conn_timeout) as connection:
-            tab_missing_sha1 = connection.table(tab_missing_sha1_name)
-            b = tab_missing_sha1.batch()
-            for image_id in stillmissing_sha1:
-                b.put(str(image_id), {'info:cdr_id': ''})
-            b.send()
-    # save the new sha1 we got
-    if len(hash_rows)!=len(str_image_ids) and [sha1 is not None for sha1 in sha1hash].count(True)>len(hash_rows): 
-        sha1_hbase=[]
-        for iid,sha1 in hash_rows:
-            sha1_hbase.append(iid)
-        new_sha1=[(str_image_ids[lid],sha1) for lid,sha1 in enumerate(sha1hash) if sha1 is not None and str_image_ids[lid] not in sha1_hbase]
-        with pool.connection(timeout=hbase_conn_timeout) as connection:
-            tab_hash = connection.table(tab_hash_name)
-            b = tab_hash.batch()
-            for image_id,sha1 in new_sha1:
-                b.put(str(image_id), {'image:hash': sha1})
-            b.send()
-    return sha1hash
-
-def saveSHA1(image_id,cdr_id,sha1hash):
-    # save in the two tables
-    # old table indexed by htid 'tab_hash'
-    if image_id and sha1hash and sha1hash!='NULL':
-        with pool.connection(timeout=hbase_conn_timeout) as connection:
-            tab_hash = connection.table(tab_hash_name)
-            tab_hash.put(str(image_id), {'image:hash': sha1hash})
-    # new table indexed by cdrid
-    if cdr_id and sha1hash and sha1hash!='NULL':
-        with pool.connection(timeout=hbase_conn_timeout) as connection:
-            tab_cdr_hash = connection.table(tab_cdrid_sha1_name)
-            tab_cdr_hash.put(str(cdr_id), {'hash:sha1': sha1hash})
 
 def getSimIds(image_id,logf=None):
     with pool.connection(timeout=hbase_conn_timeout) as connection:
@@ -278,8 +109,6 @@ def getSimIds(image_id,logf=None):
         save_missing_sim(image_id)
     return sim_ids
         
-
-# This is slow? Why?
 def saveSimPairs(sha1_sim_pairs):
     row_keys=[pair[0] for pair in sha1_sim_pairs]
     with pool.connection(timeout=hbase_conn_timeout) as connection:
@@ -385,7 +214,13 @@ def processBatch(first_row,last_row):
                     s3_url=""
                 # get SHA1
                 start_sha1=time.time()
-                sha1=getSHA1(image_id,one_row[0],f)
+                sha1=sha1_tools.get_SHA1_from_image_id_or_cdr_id(image_id,one_row[0],f)
+                if sha1hash:
+                    #print "Saving SHA1 {} for image ({},{}) in HBase".format(sha1hash,cdr_id,image_id)
+                    sha1_tools.save_SHA1_to_hbase(image_id,cdr_id,sha1hash.upper(),tab_hash_name,tab_cdrid_sha1_name)
+                else:
+                    sha1_tools.save_missing_sha1(image_id,cdr_id,tab_missing_sha1_name)
+                    #print "Could not get/compute SHA1 for {} {}.".format(image_id,cdr_id)
                 time_sha1+=time.time()-start_sha1
                 if not sha1: 
                     #time.sleep(1)
@@ -404,18 +239,11 @@ def processBatch(first_row,last_row):
                 #print sim_ids
                 start_prep_sim=time.time()
                 #f.write("Looking for sim_ids of image {}: {}\n".format(image_id,sim_ids))
-                # Process sim_ids as batch?
-                sha1_sim_ids=get_batch_SHA1_from_imageids(sim_ids[0].split(','),f)
-                ## OLD processing one by one
-                #sha1_sim_ids=[]
-                #for sim_id in sim_ids[0].split(','):
-                #    if sim_id:
-                #        #print sim_id
-                #        # Would need to query ES to get the cdr_id...
-                #        sha1_sim_ids.append(getSHA1(sim_id,None))
-                # prepare to save similarities
-                # key should be: min(sha1,sim_sha1)-max(sha1,sim_sha1)
-                # value in column info:dist is corresponding distance
+                # Process sim_ids as batch
+                sha1_sim_ids,missing_sha1s,new_sha1s=sha1_tools.get_batch_SHA1_from_imageids(sim_ids[0].split(','),tab_hash_name,f)
+                sha1_tools.save_missing_SHA1_to_hbase_missing_sha1(missing_sha1s,tab_missing_sha1_name)
+                sha1_tools.save_batch_SHA1_to_hbase_image_hash(new_sha1s,tab_hash_name)
+                # prepare to save
                 sha1_sim_pairs=[]
                 sim_dists=sim_ids[1].split(',')
                 for i,sha1_sim_id in enumerate(sha1_sim_ids):
