340cda5a5f3f1229478f89702f1f0e65596cccf2
svebor karaman <svebor.karaman@gmail.com>
2016-05-30 20:04:16 -0400

    parallelized fill sha1 sim aaron

--- hbase_ht/fillSHA1sim_aaron.py
+++ hbase_ht/fillSHA1sim_aaron.py
@@ -12,11 +12,15 @@ tab_aaron_name = 'aaron_memex_ht-images'
 tab_hash_name = 'image_hash'
 tab_missing_sha1_name = 'ht-images_missing_sha1'
 tab_missing_sim_name = 'ht-images_missing_sim_images'
-pool = happybase.ConnectionPool(size=6,host='10.1.94.57',timeout=hbase_conn_timeout)
+nb_threads = 12
+pool = happybase.ConnectionPool(size=nb_threads,host='10.1.94.57',timeout=hbase_conn_timeout)
 sha1_tools.pool = pool
 global_var = json.load(open('../../conf/global_var_all.json'))
 sha1_tools.global_var = global_var
 sha1_tools.tab_aaron_name = tab_aaron_name
+row_count = 0
+missing_sha1_count = 0
+missing_sim_count = 0
 
 batch_size = 1000
 
@@ -25,6 +29,57 @@ batch_size = 1000
 # get sha1 of row-key
 # get sha1 for each image in meta:columbia_near_dups
 # compact meta:columbia_near_dups into meta:columbia_near_dups_sha1 and maintain distances info of corresponding images from meta:columbia_near_dups_dist in meta:columbia_near_dups_sha1_dist
+
+def process_one_row(one_row):
+    global missing_sha1_count,missing_sim_count
+    row_sha1, from_url = get_row_sha1(one_row)
+    if not row_sha1:
+        #print "Could not get sha1 for image_id {}.".format(one_row[0])
+        missing_sha1_count += 1
+        # push to missig sha1
+        sha1_tools.save_missing_SHA1_to_hbase_missing_sha1([one_row[0]],tab_missing_sha1_name)
+        return
+    if from_url:
+        #print "Computed new sha1 for image_id {}.".format(one_row[0])
+        # push to image_hash
+        sha1_tools.save_SHA1_to_hbase_imagehash(one_row[0],row_sha1,tab_hash_name)
+    # add sha1 to row
+    if 'meta:columbia_near_dups' not in one_row[1].keys():
+        #print "Similar images not computed for image_id {}.".format(one_row[0])
+        missing_sim_count += 1
+        save_missing_sim_images(one_row[0])
+        return
+    sim_image_ids = [str(x) for x in one_row[1]['meta:columbia_near_dups'].split(',')]
+    sim_sha1s, missing_sim_iids, new_sha1s = sha1_tools.get_batch_SHA1_from_imageids(sim_image_ids)
+    dists = one_row[1]['meta:columbia_near_dups_dist'].split(',')
+    if new_sha1s:
+        sha1_tools.save_batch_SHA1_to_hbase_image_hash(new_sha1s,tab_hash_name)
+    if missing_sim_iids:
+        # push missing sha1
+        sha1_tools.save_missing_SHA1_to_hbase_missing_sha1(missing_sim_iids,tab_missing_sha1_name)
+        # realign dists
+        dists = [d for d,i in enumerate(dists) if sim_image_ids[i] not in missing_sim_iids]
+    unique_sim_sha1s, sim_sha1s_pos = np.unique(sim_sha1s,return_index=True)                
+    #print row_count, one_row[0], row_sha1, from_url, sim_sha1s, missing_sim_sha1s, new_sha1s
+    dists = np.asarray([np.float32(x) for x in dists])
+    sim_sha1s_sorted_pos = np.argsort(dists[sim_sha1s_pos])
+    with pool.connection() as connection:
+        tab_aaron = connection.table(tab_aaron_name)
+        tab_aaron.put(one_row[0],{'meta:sha1': str(row_sha1), 'meta:columbia_near_dups_sha1': ','.join([str(x) for x in list(unique_sim_sha1s[sim_sha1s_sorted_pos])]), 'meta:columbia_near_dups_sha1_dist': ','.join([str(x) for x in list(dists[sim_sha1s_pos[sim_sha1s_sorted_pos]])])})
+    return
+
+def process_batch_rows(list_rows):
+    batch_start = time.time()
+    for row in list_rows:
+        process_one_row(one_row)
+    tel = time.time()-batch_start
+    print "Batch from row {} done in: {}.".format(list_rows[0],tel)
+
+def process_batch_worker():
+    while True:
+        tupInp = q.get()
+        process_batch_rows(tupInp[0])
+        q.task_done()
 
 def save_missing_sim_images(image_id,tab_missing_sim_name=tab_missing_sim_name):
     with pool.connection(timeout=hbase_conn_timeout) as connection:
@@ -42,67 +97,41 @@ def get_row_sha1(row):
 if __name__ == '__main__':
     start_time = time.time()
     last_row = None
-    row_count = 0
-    missing_sha1_count = 0
-    missing_sim_count = 0
     done = False
+    list_rows = []
 
-    # # Prepare queue
-    # q = Queue()
-    # for i in range(nb_threads):
-    #     t=Thread(target=worker)
-    #     t.daemon=True
-    #     t.start()
+    # Prepare queue
+    q = Queue()
+    for i in range(nb_threads):
+        t=Thread(target=process_batch_worker)
+        t.daemon=True
+        t.start()
+
     while not done:
         try:
             with pool.connection() as connection:
                 tab_aaron = connection.table(tab_aaron_name)
                 for one_row in tab_aaron.scan(row_start=last_row):
                     row_count += 1
-                    row_sha1, from_url = get_row_sha1(one_row)
-                    if not row_sha1:
-                        #print "Could not get sha1 for image_id {}.".format(one_row[0])
-                        missing_sha1_count += 1
-                        # push to missig sha1
-                        sha1_tools.save_missing_SHA1_to_hbase_missing_sha1([one_row[0]],tab_missing_sha1_name)
-                        continue
-                    if from_url:
-                        #print "Computed new sha1 for image_id {}.".format(one_row[0])
-                        # push to image_hash
-                        sha1_tools.save_SHA1_to_hbase_imagehash(one_row[0],row_sha1,tab_hash_name)
-                    # add sha1 to row
-                    if 'meta:columbia_near_dups' not in one_row[1].keys():
-                        #print "Similar images not computed for image_id {}.".format(one_row[0])
-                        missing_sim_count += 1
-                        save_missing_sim_images(one_row[0])
-                        continue
-                    sim_image_ids = [str(x) for x in one_row[1]['meta:columbia_near_dups'].split(',')]
-                    sim_sha1s, missing_sim_iids, new_sha1s = sha1_tools.get_batch_SHA1_from_imageids(sim_image_ids)
-                    dists = one_row[1]['meta:columbia_near_dups_dist'].split(',')
-                    if new_sha1s:
-                        sha1_tools.save_batch_SHA1_to_hbase_image_hash(new_sha1s,tab_hash_name)
-                    if missing_sim_iids:
-                        # push missing sha1
-                        sha1_tools.save_missing_SHA1_to_hbase_missing_sha1(missing_sim_iids,tab_missing_sha1_name)
-                        # realign dists
-                        dists = [d for d,i in enumerate(dists) if sim_image_ids[i] not in missing_sim_iids]
-                    unique_sim_sha1s, sim_sha1s_pos = np.unique(sim_sha1s,return_index=True)                
-                    #print row_count, one_row[0], row_sha1, from_url, sim_sha1s, missing_sim_sha1s, new_sha1s
-                    dists = np.asarray([np.float32(x) for x in dists])
-                    sim_sha1s_sorted_pos = np.argsort(dists[sim_sha1s_pos])
-                    #print row_count, one_row[0], row_sha1, from_url, unique_sim_sha1s[sim_sha1s_sorted_pos], dists[sim_sha1s_pos[sim_sha1s_sorted_pos]]
-                    tab_aaron.put(one_row[0],{'meta:sha1': str(row_sha1), 'meta:columbia_near_dups_sha1': ','.join([str(x) for x in list(unique_sim_sha1s[sim_sha1s_sorted_pos])]), 'meta:columbia_near_dups_sha1_dist': ','.join([str(x) for x in list(dists[sim_sha1s_pos[sim_sha1s_sorted_pos]])])})
+                    list_rows.append(one_row)
                     if row_count%(batch_size)==0:
-                        tel = time.time()-start_time
-                        print "Scanned {} rows so far (misssing sha1: {}, sim: {}). Average time per row is: {}. Total time is: {}.".format(row_count,missing_sha1_count,missing_sim_count,tel/row_count,tel)
+                        print "Scanned {} rows so far. Pushing batch starting from row {}.".format(row_count,list_rows[0])
+                        q.put((list_rows,))
+                        list_rows = []
                         sys.stdout.flush()
-                        #time.sleep(60)
                 done = True
+                if list_rows:
+                    # push last batch
+                    print "Scanned {} rows so far. Pushing batch starting from row {}.".format(row_count,list_rows[0])
+                    q.put((list_rows,))
+                    list_rows = []
+                    sys.stdout.flush()
         except Exception as inst:
             print "[Caught error] {}\n".format(inst)
             exc_type, exc_obj, exc_tb = sys.exc_info()  
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print "{} in {} line {}.\n".format(exc_type, fname, exc_tb.tb_lineno)
             time.sleep(2)
-
-
+        q.join()
+        tel = time.time()-start_time
+        print "Scanned {} rows total (misssing sha1: {}, sim: {}). Average time per row is: {}. Total time is: {}.".format(row_count,missing_sha1_count,missing_sim_count,tel/row_count,tel)
