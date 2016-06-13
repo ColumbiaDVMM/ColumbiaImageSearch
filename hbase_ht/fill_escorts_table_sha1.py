1035d02ddf5f287e8f6095b0b0b9f63c69510518
Svebor Karaman <svebor.karaman@gmail.com>
2016-06-13 04:55:45 +0000

    script works but we get very few sha1

--- hbase_ht/fill_escorts_table_sha1.py
+++ hbase_ht/fill_escorts_table_sha1.py
@@ -10,7 +10,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../memex_tools'))
 
 hbase_conn_timeout = None
-nb_threads = 1
+nb_threads = 2
 pool = happybase.ConnectionPool(size=nb_threads,host='10.1.94.57',timeout=hbase_conn_timeout)
 global_var = json.load(open('../../conf/global_var_all.json'))
 
@@ -25,9 +25,10 @@ def get_sha1(tab_sha1_name,rows_ids):
     sha1s_list = [None]*len(rows_ids) 
     with pool.connection() as connection:
         tab_sha1 = connection.table(tab_sha1_name)
-        rows_sha1 = tab_sha1.rows(rows_ids)
+        rows_sha1 = tab_sha1.rows(rows_ids,columns=["meta:sha1","meta:columbia_near_dups_sha1","meta:columbia_near_dups_sha1_dist"])
         for row in rows_sha1:
             pos = rows_ids.index(row[0])
+            print row
             im_sha1 = None
             nd_sha1 = None
             ndd_sha1 = None
@@ -38,6 +39,7 @@ def get_sha1(tab_sha1_name,rows_ids):
             if "meta:columbia_near_dups_sha1_dist" in row[1]:
                 ndd_sha1 = row[1]["meta:columbia_near_dups_sha1_dist"]
             sha1s_list[pos]=[im_sha1,nd_sha1,ndd_sha1]
+            print sha1s_list[pos]
     return sha1s_list
 
 def get_image_ids_url(batch):
@@ -48,10 +50,10 @@ def get_image_ids_url(batch):
         url = None
         if "info:crawl_data.image_id" in row[1]:
             iid = row[1]["info:crawl_data.image_id"].strip()
-        if "info:obj_stored_url" in row[1]:
-            url = row[1]["info:obj_stored_url"].strip()
-        if iid and url:
-            list_images_ids_url.append([row[0].strip(),iid,url])
+        #if "info:obj_stored_url" in row[1]:
+        #    url = row[1]["info:obj_stored_url"].strip()
+        if iid:
+            list_images_ids_url.append([row[0].strip(),iid])
     return list_images_ids_url
 
 def read_batch(tab_in_name,start_row,batch_size):
@@ -77,12 +79,21 @@ def write_batch(batch,tab_out_name):
         batch_write.send()
 
 def fill_sha1_info_onebatch(tab_escimg_name,tab_sha1_name,batch_size,start_row):
-    batch,c_read,new_start_row = read_batch(tab_in_name,start_row,batch_size)
+    batch,c_read,new_start_row = read_batch(tab_escimg_name,start_row,batch_size)
     if c_read>0:
+        print "Read {} rows. Processing.".format(c_read)
+        sys.stdout.flush()
         list_images_ids_url = get_image_ids_url(batch)
-        sha1s_list = get_sha1(tab_sha1_name,[x[1] for x in list_images_ids_url])
+        print "Got {} image ids.".format(len(list_images_ids_url))
+        image_ids = [x[1] for x in list_images_ids_url]
+        #print image_ids
+        sha1s_list = get_sha1(tab_sha1_name,image_ids)
+        #print sha1s_list
         # sha1s_list[pos]=[im_sha1,nd_sha1,ndd_sha1]
-        out_batch = [x[0],{"info:sha1": sha1s_list[i][0], "info:columbia_near_dups_sha1": sha1s_list[i][1],  "info:columbia_near_dups_sha1_dist": sha1s_list[i][2]} for i,x in enumerate(list_images_ids_url) if sha1s_list[i] and sha1s_list[i]!=[None,None,None]]
+        #out_batch = [(x[0],{"info:sha1": sha1s_list[i][0], "info:columbia_near_dups_sha1": sha1s_list[i][1],  "info:columbia_near_dups_sha1_dist": sha1s_list[i][2]}) for i,x in enumerate(list_images_ids_url) if sha1s_list[i] and sha1s_list[i]!=[None,None,None]]
+        out_batch = [(x[0],{"info:sha1": sha1s_list[i][0]}) for i,x in enumerate(list_images_ids_url) if sha1s_list[i] and sha1s_list[i]!=[None,None,None]]
+        print "Saving {} rows.".format(len(out_batch))
+        print out_batch
         write_batch(out_batch,tab_escimg_name)
         return c_read,new_start_row
     else:
@@ -91,14 +102,18 @@ def fill_sha1_info_onebatch(tab_escimg_name,tab_sha1_name,batch_size,start_row):
 def fill_sha1_info(tab_escimg_name,tab_sha1_name,batch_size,start_row):
     start_time = time.time()
     done = False
+    count_total = 0
     while not done:
         count_read,new_start_row = fill_sha1_info_onebatch(tab_escimg_name,tab_sha1_name,batch_size,start_row)
-        if new_start_row==None:
+        if new_start_row==start_row:
             print "Did not get any rows. Leaving."
             done = True
+            break
+        count_total+=count_read
+        start_row=new_start_row
     tel = time.time()-start_time
-    if count_read>0:
-        print "Read {} rows total. Average time per row is: {}. Total time is: {}.".format(count_read,tel/count_read,tel)
+    if count_total>0:
+        print "Read {} rows total. Average time per row is: {}. Total time is: {}.".format(count_total,tel/count_total,tel)
     
 
 if __name__ == '__main__':
