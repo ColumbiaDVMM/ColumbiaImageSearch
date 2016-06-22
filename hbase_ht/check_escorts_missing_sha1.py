52cf63cbf313603a969434e1804225f1a32ff2c2
Svebor Karaman <svebor.karaman@gmail.com>
2016-06-22 23:09:03 +0000

    hbase check

--- hbase_ht/check_escorts_missing_sha1.py
+++ hbase_ht/check_escorts_missing_sha1.py
@@ -13,6 +13,7 @@ sha1_tools.pool = pool
 tab_escorts_images_name = 'escorts_images_cdrid_infos'
 tab_escorts_missing_sha1_name = 'escorts_images_missing_sha1'
 tab_hash_name = 'image_id_sha1'
+tab_image_cache_name = 'image_cache'
 
 global_var = json.load(open('../../conf/global_var_all.json'))
 sha1_tools.global_var = global_var
@@ -24,10 +25,22 @@ tab = connection.table(tab_escorts_missing_sha1_name)
 show=10000
 
 count_ok_from_tab_hash=0
+count_ok_image_content=0
 count_tot=0
 
 done=False
 start_row=None
+
+def get_image_content(sha1):
+    cache_row = None
+    image_content = None
+    if image_id:
+        with pool.connection(timeout=hbase_conn_timeout) as connection:
+            tab_cache = connection.table(tab_image_cache_name)
+            cache_row = tab_cache.row(sha1)
+    if cache_row:
+        image_content = cache_row['image:binary']
+    return image_content
 
 while not done:
   try:
@@ -35,9 +48,14 @@ while not done:
       #sha1=row[1]['hash:sha1']
       if 'info:image_id' in row[1].keys():
         image_id = row[1]['info:image_id']
-        sha1 = get_SHA1_from_hbase_imagehash(image_id,tab_hash_name)
+        sha1 = sha1_tools.get_SHA1_from_hbase_imagehash(image_id,tab_hash_name)
         if sha1_tools.check_sha1(sha1):
-          print "Managed to get sha1 {} from image_id {} for cdr_id {}.".format(sha1,image_id,row[0])
+          image_content = get_image_content(sha1)
+          if image_content:
+              print "Got sha1 {} and image content from image_id {} for cdr_id {}.".format(sha1,image_id,row[0])
+              count_ok_image_content+=1
+          else:
+              print "Got sha1 {} from image_id {} for cdr_id {}.".format(sha1,image_id,row[0])
           count_ok_from_tab_hash+=1
       start_row=row[0]
       count_tot+=1
@@ -48,4 +66,4 @@ while not done:
      print inst
      time.sleep(2)
 
-print count_ok_from_tab_hash,count_tot
+print "Got a total of {} sha1 and {} image content from {} missing sha1 initially.".format(count_ok_from_tab_hash,count_ok_image_content,count_tot)
--- hbase_ht/fix_sha1_aaron.py
+++ hbase_ht/fix_sha1_aaron.py
@@ -20,14 +20,15 @@ tab = connection.table(tab_aaron_name)
 show=10000
 
 count_null=0
+count_null_sim=0
 count_tot=0
 
 done=False
-start_row='109492187'
+start_row=None
 
 while not done:
   try:
-    for row in tab.scan(row_start=start_row,columns=['meta:sha1']):
+    for row in tab.scan(row_start=start_row,columns=['meta:sha1','meta:columbia_near_dups_sha1']):
       #sha1=row[1]['hash:sha1']
       sha1=row[1]['meta:sha1']
       start_row=row[0]
@@ -35,13 +36,19 @@ while not done:
       if count_tot%show==0:
           print "Scanned {} rows, currently at {}.".format(count_tot,row[0])
       if not sha1_tools.check_sha1(sha1):
-          print "Corrupted sha1. {}".format(row)
+          print "Corrupted sha1 in row {}".format(row[0])
           sys.stdout.flush()
           #tab.delete(row[0])
           count_null+=1
+      for sim_sha1 in row[1]['meta:columbia_near_dups_sha1']:
+          if not sha1_tools.check_sha1(sim_sha1):
+              print "Corrupted sim sha1 in row {}".format(row[0])
+              sys.stdout.flush()
+              #tab.delete(row[0])
+              count_null_sim+=1
     done=True
   except Exception as inst:
      print inst
      time.sleep(2)
 
-print count_null,count_tot
+print count_null,count_null_sim,count_tot
