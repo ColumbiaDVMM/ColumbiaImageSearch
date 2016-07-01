450d0ee64505e52dda12f0938855f0f315a8b42a
Svebor Karaman <svebor.karaman@gmail.com>
2016-07-01 20:32:56 +0000

    test precomp OK

--- cu_image_search/hashing/test_precomp.py
+++ cu_image_search/hashing/test_precomp.py
@@ -9,7 +9,7 @@ if __name__=="__main__":
     base_updatepath = "/home/ubuntu/memex/update/"
     feature_num = 4096
     bits_num = 256
-    list_feats_id = [1, 1000, 10000, 100000, 1000000]
+    list_feats_id = [1, 1000, 10000, 100000, 1000000, 10000000]
     with open(query_precomp_fn,"wb") as f_pre:
         for feat_id in list_feats_id:
             f_pre.write(struct.pack('i',feat_id))
@@ -32,8 +32,10 @@ if __name__=="__main__":
     hashcodes = []
     with open(hashcodes_fn,"rb") as f_prehash:
         for i in range(len(list_feats_id)):
-            hashcodes.append(np.frombuffer(f_prefeats.read(bits_num/32),dtype=np.uint8))
+            hashcodes.append(np.frombuffer(f_prehash.read(bits_num/8),dtype=np.uint8))
     for i in range(len(list_feats_id)):
         print hashcodes[i]
+        tmp = np.unpackbits(hashcodes[i])
+        print tmp.shape, tmp
         print np.max(hashcodes[i])
         print hashcodes[i].shape
