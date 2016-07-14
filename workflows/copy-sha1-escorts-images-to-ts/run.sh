da6c7a8bfe9b68da0cc438e5d4bcbfedb7ae0359
svebor karaman <svebor.karaman@gmail.com>
2016-07-14 10:58:45 -0400

    copy sha1 workflow

--- workflows/copy-sha1-escorts-images-to-ts/run.sh
+++ workflows/copy-sha1-escorts-images-to-ts/run.sh
@@ -2,6 +2,6 @@
  --master yarn-client \
 --executor-memory 20g  --executor-cores 5  --num-executors 80 \
 --jars spark-examples_2.10-2.0.0-SNAPSHOT.jar,random-0.0.1-SNAPSHOT-shaded.jar  \
---py-files ../python-lib.zip,../happybase.zip,image_dl.py,../thriftpy.zip \
-sha1-for-escorts-images.py  \
+--py-files python-lib.zip \
+copy-sha1-escorts-images-to-ts.py  \
  $@
