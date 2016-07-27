2e43dddb406c8c5716c3dcfa209621bfa0b3e8c4
Svebor Karaman <svebor.karaman@gmail.com>
2016-07-27 19:31:09 +0000

    testing many columns issue

--- scripts/tests/test_get_sim.py
+++ scripts/tests/test_get_sim.py
@@ -3,7 +3,7 @@ import happybase
 if __name__=="__main__":
     tab_image = 'escorts_images_similar_row'    
     conn = happybase.Connection(host='10.1.94.57')
-    image_sha1s = ['1000013C0A38D8DACAEC31360AFAFEB5DC3D712B']
+    image_sha1s = '1000013C0A38D8DACAEC31360AFAFEB5DC3D712B'
     table = conn.table(tab_image)
-    for row in table.rows(image_sha1s,columns=['s']):
-        print len(row[1].keys()),[x.split(':')[-1] for x in row[1].keys()]
+    row = table.row(image_sha1s,columns=['s'])
+    print len(row.keys()),[x.split(':')[-1] for x in row.keys()]
--- scripts/tests/test_many_column.py
+++ scripts/tests/test_many_column.py
@@ -1,12 +1,18 @@
 import happybase
+import hashlib
 
 if __name__=="__main__":
-	conn = happybase.Connection(host='10.1.94.57')
+    conn = happybase.Connection(host='10.1.94.57')
     tab_name = 'test_many_columns'
-    row_key = 'KEY'
-    conn.create_table(tab_name,{'c': dict()})
-    table = conn.table(tab_image)
-    for cq in range(1000):
-    	table.put(row_key, {'c:{}'.format(cq): cq})
+    row_key = 'KEY_SHA1'
+    try:
+        conn.create_table(tab_name,{'c': dict()})
+    except Exception as inst:
+	print inst
+    table = conn.table(tab_name)
+    for cq in range(100000,1000000):
+        hasher = hashlib.sha1()
+        hasher.update(b'{}'.format(cq))    
+    	table.put(row_key, {'c:{}'.format(hasher.hexdigest()): str(cq)})
     	row = table.row(row_key)
         print cq+1,len(row.keys())
