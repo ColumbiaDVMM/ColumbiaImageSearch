a350ceafe8b8f5f5478552c3c0e1653dc2591eaa
svebor karaman <svebor.karaman@gmail.com>
2016-09-05 14:47:24 -0400

    double b64

--- cu_image_search/www/test_api.py
+++ cu_image_search/www/test_api.py
@@ -22,11 +22,11 @@ def get_b64(url):
     return b64_from_data
 
 if __name__ == "__main__":
-    sampleURL = 'https://s3.amazonaws.com/roxyimages/939446e1543d2a7ebf73b438f6f21dbb6e71f04a.jpg'
+    sampleURLs = ['https://s3.amazonaws.com/roxyimages/939446e1543d2a7ebf73b438f6f21dbb6e71f04a.jpg','https://s3.amazonaws.com/roxyimages/f99f89526bdf335483c9776c73a059ead1f16d27.jpg']
     apiURL = "http://127.0.0.1:5000/cu_image_search/byB64_nocache"
     #apiURL = "https://isi.memexproxy.com/ESCORTS/cu_image_search/byB64_nocache" # would need auth?
-    query_b64 = get_b64(sampleURL)
+    query_b64 = [get_b64(sampleURL) for sampleURL in sampleURLs]
     #print query_b64
-    res = requests.post(apiURL, data={"data":query_b64})
+    res = requests.post(apiURL, data={"data":','.join(query_b64)})
     print res
     print res.content
