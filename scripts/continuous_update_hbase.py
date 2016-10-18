188afd6dd056e0ae34607ca1dea8a74f4f247c42
Svebor Karaman <svebor.karaman@gmail.com>
2016-10-18 20:16:30 +0000

    refresh API

--- scripts/continuous_update_hbase.py
+++ scripts/continuous_update_hbase.py
@@ -7,7 +7,7 @@ import cu_image_search
 from cu_image_search.update import updater_hbase
 
 lasttime = 0
-interval = 60
+interval = 300
 
 if __name__=="__main__":
     """ Run updater based on `conf_file` given as parameter
@@ -24,7 +24,9 @@ if __name__=="__main__":
             if time_lapse < interval:
                 if up_obj.has_indexed:
                     # we should refresh the API
+                    print('[continuous_update_hbase] refreshing the API.')
                     up_obj.refresh_API()
+                    print('[continuous_update_hbase] refreshed the API.')
                     up_obj.has_indexed = False
                 print('[continuous_update_hbase] sleeping for {} seconds...'.format(interval-time_lapse))
                 sys.stdout.flush()
