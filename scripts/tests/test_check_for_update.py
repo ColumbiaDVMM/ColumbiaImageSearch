bf0118840a713ef67ce4b2f700ee0992dfc94d56
Svebor Karaman <svebor.karaman@gmail.com>
2016-11-04 16:12:36 +0000

    batch+size explicitly in get_next_batch

--- scripts/tests/test_check_for_update.py
+++ scripts/tests/test_check_for_update.py
@@ -5,7 +5,7 @@ conn = happybase.Connection(host='10.1.94.57')
 table_updateinfos = conn.table(tab_name)
 index_batches = []
 
-for row in table_updateinfos.scan(row_start='index_update_', row_stop='index_update_~'):
+for row in table_updateinfos.scan(row_start='index_update_', row_stop='index_update_~', batch_size=500):
     if "info:indexed" not in row[1] and 'info:started' not in row[1] and 'info:corrupted' not in row[1]:
         print("Adding update: {}".format(row[0]))
         index_batches.append((row[0], row[1]["info:list_sha1s"]))
