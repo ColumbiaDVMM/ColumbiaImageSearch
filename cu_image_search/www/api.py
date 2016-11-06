1c90d31342019b552642d8f53e65b6184467a27d
svebor karaman <svebor.karaman@gmail.com>
2016-11-06 17:28:22 -0500

    order API status

--- cu_image_search/www/api.py
+++ cu_image_search/www/api.py
@@ -320,13 +320,13 @@ class APIResponder(Resource):
 
     def status(self):
         status_dict = {'status': 'OK'}
+        status_dict['API_start_time'] = self.start_time.isoformat(' ')
+        status_dict['API_uptime'] = str(datetime.now()-self.start_time)
         if self.searcher.indexer.last_refresh:
             status_dict['last_refresh_time'] = self.searcher.indexer.last_refresh.isoformat(' ')
         else:
             status_dict['last_refresh_time'] = self.searcher.indexer.last_refresh
         status_dict['indexed_images'] = len(self.searcher.indexer.sha1_featid_mapping)
-        status_dict['API_start_time'] = self.start_time.isoformat(' ')
-        status_dict['API_uptime'] = str(datetime.now()-self.start_time)
         return status_dict
 
 
