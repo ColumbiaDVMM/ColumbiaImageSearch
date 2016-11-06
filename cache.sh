29e1cddbf36389af0e3a832a409949d614036a7e
Svebor Karaman <svebor.karaman@gmail.com>
2016-11-06 22:02:21 +0000

    fix shebang

--- cache.sh
+++ cache.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 killall vmtouch
 #/home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/
 /home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/update/indexing/sha1_mapping /home/ubuntu/memex/update/indexing/hash_bits/ /home/ubuntu/memex/update/indexing/comp_features/ /home/ubuntu/memex/update/indexing/comp_idx/
