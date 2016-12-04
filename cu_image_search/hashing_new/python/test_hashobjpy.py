954a0b9a4919dda95d398c7efb24d9fee2f64a8d
Svebor Karaman <svebor.karaman@gmail.com>
2016-12-04 04:23:46 +0000

    load model ok

--- cu_image_search/hashing_new/python/test_hashobjpy.py
+++ cu_image_search/hashing_new/python/test_hashobjpy.py
@@ -4,11 +4,17 @@ import sys
 if __name__ == "__main__":
     hasher = new_HasherObjectPy()
     # setters are not working?
+    up_path = HasherObjectPy_get_base_updatepath(hasher)
+    print(up_path)
     HasherObjectPy_set_base_updatepath(hasher, "/home/ubuntu/memex/update/indexing/")
-    HasherObjectPy_set_base_modelpath(hasher, "/home/ubuntu/memex/update/indexing/")
+    up_path = HasherObjectPy_get_base_updatepath(hasher)
+    print(up_path)
+    HasherObjectPy_set_base_modelpath(hasher, "/home/ubuntu/memex/data/")
+    HasherObjectPy_set_paths(hasher)
     status = HasherObjectPy_read_update_files(hasher)
     if status != 0:
         print("Hasher was not able to read update")
 	sys.exit(-1)
-    HasherObjectPy_load_hashcodes(hasher)
     HasherObjectPy_load_itq_model(hasher)
+    HasherObjectPy_fill_data_nums_accum(hasher)
+    HasherObjectPy_load_hashcodes(hasher)
