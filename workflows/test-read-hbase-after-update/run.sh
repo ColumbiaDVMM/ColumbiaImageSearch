931a0a7568977737472352fde6f6344d61e1026d
svebor karaman <svebor.karaman@gmail.com>
2017-03-07 14:24:58 -0500

    remove ES package

--- workflows/test-read-hbase-after-update/run.sh
+++ workflows/test-read-hbase-after-update/run.sh
@@ -6,7 +6,7 @@
 --conf spark.eventLog.dir=hdfs://memex/user/spark/applicationHistory \
 --conf spark.yarn.historyServer.address=memex-spark-master.xdata.data-tactics-corp.com:18080 \
 --conf spark.logConf=true \
---jars elasticsearch-hadoop-2.3.2.jar,spark-examples_2.10-2.0.0-SNAPSHOT.jar,random-0.0.1-SNAPSHOT-shaded.jar  \
+--jars spark-examples_2.10-2.0.0-SNAPSHOT.jar,random-0.0.1-SNAPSHOT-shaded.jar  \
 --py-files python-lib.zip \
 test_read_hbase.py   \
 $@
