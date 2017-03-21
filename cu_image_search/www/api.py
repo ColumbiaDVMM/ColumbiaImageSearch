34bf7a2f644912a6a340be6d9f736a3ed31461e8
svebor karaman <svebor.karaman@gmail.com>
2017-03-20 20:16:38 -0400

    merge from master and set dev in incremental update and api

--- cu_image_search/www/api.py
+++ cu_image_search/www/api.py
@@ -35,9 +35,9 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   return response
 
-global_conf_file = '../../conf/global_var_remotehbase_release.json'
+#global_conf_file = '../../conf/global_var_remotehbase_release.json'
 #global_conf_file = '../../conf/global_var_remotehbase.json'
-#global_conf_file = '../../conf/global_var_remotehbase_dev.json'
+global_conf_file = '../../conf/global_var_remotehbase_dev.json'
 global_searcher = None
 global_start_time = None
 
