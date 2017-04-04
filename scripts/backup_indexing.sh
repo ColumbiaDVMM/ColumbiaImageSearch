1e7f3bf565c25ab46d1a9007c0ecedaba4e208df
Svebor Karaman <svebor.karaman@gmail.com>
2017-04-04 17:58:17 -0400

    backup only necessary files

--- scripts/backup_indexing.sh
+++ scripts/backup_indexing.sh
@@ -2,9 +2,13 @@
 backup_date=$(date +%Y-%m-%d)
 index_dir=/srv/skaraman/cu_image_search_ht/update/indexing
 backup_dir=/mnt/isi/backup_indexing/indexing-${backup_date}
+necessary_files=("update_list*.txt" "sha1_mapping" "comp_features" "comp_idx" "hash_bits")
 if [ ! -d "$backup_dir" ]; then
   echo "Backing up indexing at ${backup_dir}"
-  cmd="cp -r ${index_dir} ${backup_dir}"
-  echo $cmd
-  $($cmd)
+  for onefile in ${necessary_files[@]}
+  do
+    cmd="cp -r ${index_dir}/${onefile} ${backup_dir}"
+    echo $cmd
+    $($cmd)
+  done
 fi
