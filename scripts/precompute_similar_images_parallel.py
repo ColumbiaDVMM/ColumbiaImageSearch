f3f34da2ed61ab85a8d6ec03e92c1c0061ecf66c
svebor karaman <svebor.karaman@gmail.com>
2017-04-13 21:06:05 -0400

    finalizer sends end message

--- scripts/precompute_similar_images_parallel.py
+++ scripts/precompute_similar_images_parallel.py
@@ -16,6 +16,11 @@ nb_workers = 16
 time_sleep = 60
 
 # should we try/except main loop of producer, consumer and finalizer?
+def end_producer(queueIn):
+    for i in range(nb_workers):
+        # sentinel value, one for each worker
+        queueIn.put((None, None, None))
+
 
 def producer(global_conf_file, queueIn, queueProducer):
     print "[producer: log] Started a producer worker (pid: {}) at {}".format(os.getpid(), get_now())
@@ -24,28 +29,37 @@ def producer(global_conf_file, queueIn, queueProducer):
     print "[producer: log] Producer worker ready at {}".format(get_now())
     queueProducer.put("Producer ready")
     while True:
-        start_get_batch = time.time()
-        update_id, str_list_sha1s = searcher_producer.indexer.get_next_batch_precomp_sim()
-        #queueProducer.put("Producer got batch")
-        print "[producer: log] Got batch in {}s at {}".format(time.time() - start_get_batch, get_now())
-        sys.stdout.flush()
-        if update_id is None:
-            print "[producer: log] No more update to process."
-            queueIn.close()
-            break
-        else:
-            start_precomp = time.time()
-            # check that sha1s of batch have no precomputed similarities already in sha1_infos table
-            valid_sha1s, not_indexed_sha1s, precomp_sim_sha1s = check_indexed_noprecomp(searcher_producer, str_list_sha1s.split(','))
-            # should we split valid_sha1s in batches of 100 or something smaller than 10K currently?
-            searcher_producer.indexer.write_batch([(update_id, {searcher_producer.indexer.precomp_start_marker: 'True'})], searcher_producer.indexer.table_updateinfos_name)
-            # push updates to be processed in queueIn
-            # https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Queue.qsize
-            # qsize raises NotImplemented Error on OS X...
-            #print "[producer: log] Pushing update {} in queue containing {} items at {}.".format(update_id, queueIn.qsize(), get_now())
-            print "[producer: log] Pushing update {} at {}.".format(update_id, get_now())
+        try:
+            start_get_batch = time.time()
+            update_id, str_list_sha1s = searcher_producer.indexer.get_next_batch_precomp_sim()
+            #queueProducer.put("Producer got batch")
+            print "[producer: log] Got batch in {}s at {}".format(time.time() - start_get_batch, get_now())
             sys.stdout.flush()
-            queueIn.put((update_id, valid_sha1s, start_precomp))
+            if update_id is None:
+                print "[producer: log] No more update to process."
+                return end_producer(queueIn)
+            else:
+                start_precomp = time.time()
+                # check that sha1s of batch have no precomputed similarities already in sha1_infos table
+                valid_sha1s, not_indexed_sha1s, precomp_sim_sha1s = check_indexed_noprecomp(searcher_producer, str_list_sha1s.split(','))
+                # should we split valid_sha1s in batches of 100 or something smaller than 10K currently?
+                searcher_producer.indexer.write_batch([(update_id, {searcher_producer.indexer.precomp_start_marker: 'True'})], searcher_producer.indexer.table_updateinfos_name)
+                # push updates to be processed in queueIn
+                # https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Queue.qsize
+                # qsize raises NotImplemented Error on OS X...
+                #print "[producer: log] Pushing update {} in queue containing {} items at {}.".format(update_id, queueIn.qsize(), get_now())
+                print "[producer: log] Pushing update {} at {}.".format(update_id, get_now())
+                sys.stdout.flush()
+                queueIn.put((update_id, valid_sha1s, start_precomp))
+        except Exception as inst:
+            print "[producer: error] Error at {}. Leaving. Error was: {}".format(get_now(), inst)
+            return end_producer(queueIn)
+
+
+def end_consumer(queueIn, queueOut):
+    queueIn.task_done()
+    queueOut.put((None, None, None, None, None, None))
+            
 
 
 def consumer(global_conf_file, queueIn, queueOut, queueConsumer):
@@ -56,25 +70,38 @@ def consumer(global_conf_file, queueIn, queueOut, queueConsumer):
     queueConsumer.put("Consumer ready")
     sys.stdout.flush()
     while True:
-        ## reads from queueIn
-        print "[consumer: log] Consumer worker (pid: {}) waiting for update at {}".format(os.getpid(), get_now())
-        sys.stdout.flush()
-        update_id, valid_sha1s, start_precomp = queueIn.get()
-        ## search
-        print "[consumer: log] Consumer worker (pid: {}) computing similarities for {} valid sha1s of update {} at {}".format(os.getpid(), len(valid_sha1s), update_id, get_now())
-        sys.stdout.flush()
-        start_search = time.time()
-        # precompute similarities using searcher 
-        # for v1 check_indexed_noprecomp
-        #simname, corrupted = searcher_consumer.search_from_sha1_list_get_simname(valid_sha1s, update_id)
-        simname, corrupted = searcher_consumer.search_from_listid_get_simname(valid_sha1s, update_id)
-        elapsed_search = time.time() - start_search
-        print "[consumer: log] Processed update {} at {}. Search performed in {}s.".format(update_id, get_now(), elapsed_search)
-        sys.stdout.flush()
-        ## push to queueOut
-        queueIn.task_done()
-        queueOut.put((update_id, simname, valid_sha1s, corrupted, start_precomp, elapsed_search))
+        try:
+            ## reads from queueIn
+            print "[consumer: log] Consumer worker (pid: {}) waiting for update at {}".format(os.getpid(), get_now())
+            sys.stdout.flush()
+            update_id, valid_sha1s, start_precomp = queueIn.get()
+            if update_id is None:
+                # declare worker ended
+                print "[consumer: log] Consumer worker (pid: {}) ending at {}".format(os.getpid(), get_now())
+                return end_consumer(queueIn, queueOut)
+            ## search
+            print "[consumer: log] Consumer worker (pid: {}) computing similarities for {} valid sha1s of update {} at {}".format(os.getpid(), len(valid_sha1s), update_id, get_now())
+            sys.stdout.flush()
+            start_search = time.time()
+            # precompute similarities using searcher 
+            # for v1 check_indexed_noprecomp
+            #simname, corrupted = searcher_consumer.search_from_sha1_list_get_simname(valid_sha1s, update_id)
+            simname, corrupted = searcher_consumer.search_from_listid_get_simname(valid_sha1s, update_id)
+            elapsed_search = time.time() - start_search
+            print "[consumer: log] Processed update {} at {}. Search performed in {}s.".format(update_id, get_now(), elapsed_search)
+            sys.stdout.flush()
+            ## push to queueOut
+            queueIn.task_done()
+            queueOut.put((update_id, simname, valid_sha1s, corrupted, start_precomp, elapsed_search))
+        except Exception as inst:
+            print "[consumer: error] Consumer worker (pid: {}) caught error at {}. Leaving. Error was {}".format(os.getpid(), get_now(), inst)
+            return end_consumer(queueIn, queueOut)
 
+
+def end_finalizer(queueOut, queueFinalizer):
+    print "Pushing 'Finalizer ended' to queueFinalizer"
+    queueFinalizer.put("Finalizer ended")
+    queueOut.close()
 
 
 def finalizer(global_conf_file, queueOut, queueFinalizer):
@@ -83,49 +110,63 @@ def finalizer(global_conf_file, queueOut, queueFinalizer):
     searcher_finalizer = searcher_hbaseremote.Searcher(global_conf_file)
     print "[finalizer: log] Finalizer worker (pid: {}) ready at {}".format(os.getpid(), get_now())
     queueFinalizer.put("Finalizer ready")
+    count_workers_ended = 0
     while True:
-        ## Read from queueOut
-        print "[finalizer: log] Finalizer worker (pid: {}) waiting for an update at {}".format(os.getpid(), get_now())
-        update_id, simname, valid_sha1s, corrupted, start_precomp, elapsed_search = queueOut.get()
-        print "[finalizer: log] Finalizer worker (pid: {}) got update {} to finalize at {}".format(os.getpid(), update_id, get_now())
-        sys.stdout.flush()
-        ## Push computed similarities
-        # format for saving in HBase:
-        # - batch_sim: should be a list of sha1 row key, dict of "s:similar_sha1": dist_value
-        # - batch_mark_precomp_sim: should be a list of sha1 row key, dict of precomp_sim_column: True
-        batch_sim, batch_mark_precomp_sim = format_batch_sim(simname, valid_sha1s, corrupted, searcher_finalizer)
+        try:
+            ## Read from queueOut
+            print "[finalizer: log] Finalizer worker (pid: {}) waiting for an update at {}".format(os.getpid(), get_now())
+            update_id, simname, valid_sha1s, corrupted, start_precomp, elapsed_search = queueOut.get()
+            if update_id is None:
+                count_workers_ended += 1
+                print "[finalizer: log] {} consumer workers ended out of {} at {}.".format(count_workers_ended, nb_workers, get_now())
+                queueOut.task_done()
+                if count_workers_ended == nb_workers:
+                    # fully done
+                    print "[finalizer: log] All consumer workers ended at {}. Leaving.".format(get_now())
+                    return end_finalizer(queueOut, queueFinalizer)
+                continue
+            print "[finalizer: log] Finalizer worker (pid: {}) got update {} to finalize at {}".format(os.getpid(), update_id, get_now())
+            sys.stdout.flush()
+            ## Push computed similarities
+            # format for saving in HBase:
+            # - batch_sim: should be a list of sha1 row key, dict of "s:similar_sha1": dist_value
+            # - batch_mark_precomp_sim: should be a list of sha1 row key, dict of precomp_sim_column: True
+            batch_sim, batch_mark_precomp_sim = format_batch_sim(simname, valid_sha1s, corrupted, searcher_finalizer)
 
-        # push similarities to HBI_table_sim (escorts_images_similar_row_dev) using searcher.indexer.write_batch
-        searcher_finalizer.indexer.write_batch(batch_sim, searcher_finalizer.indexer.table_sim_name)
-        # push to weekly update table for Amandeep to integrate in DIG
-        week, year = get_week_year()
-        weekly_sim_table_name = searcher_finalizer.indexer.table_sim_name+"_Y{}W{}".format(year, week)
-        print "[finalizer: log] weekly table name: {}".format(weekly_sim_table_name)
-        weekly_sim_table = searcher_finalizer.indexer.get_create_table(weekly_sim_table_name, families={'s': dict()})
-        searcher_finalizer.indexer.write_batch(batch_sim, weekly_sim_table_name)
+            # push similarities to HBI_table_sim (escorts_images_similar_row_dev) using searcher.indexer.write_batch
+            searcher_finalizer.indexer.write_batch(batch_sim, searcher_finalizer.indexer.table_sim_name)
+            # push to weekly update table for Amandeep to integrate in DIG
+            week, year = get_week_year()
+            weekly_sim_table_name = searcher_finalizer.indexer.table_sim_name+"_Y{}W{}".format(year, week)
+            print "[finalizer: log] weekly table name: {}".format(weekly_sim_table_name)
+            weekly_sim_table = searcher_finalizer.indexer.get_create_table(weekly_sim_table_name, families={'s': dict()})
+            searcher_finalizer.indexer.write_batch(batch_sim, weekly_sim_table_name)
 
-        ## Mark as done
-        # mark precomp_sim true in escorts_images_sha1_infos_dev
-        searcher_finalizer.indexer.write_batch(batch_mark_precomp_sim, searcher_finalizer.indexer.table_sha1infos_name)
-        # mark info:precomp_finish in escorts_images_updates_dev
-        if not corrupted: # do not mark finished if we faced some issue? mark as corrupted?
-            searcher_finalizer.indexer.write_batch([(update_id, {searcher_finalizer.indexer.precomp_end_marker: 'True'})],
-                                                   searcher_finalizer.indexer.table_updateinfos_name)
-        print "[finalizer: log] Finalize update {} at {} in {}s total.".format(update_id, get_now(), time.time() - start_precomp)
-        sys.stdout.flush()
-        ## Cleanup
-        if simname:
-            try:
-                # remove simname 
-                os.remove(simname)
-                # remove features file
-                featfirst = simname.split('sim')[0]
-                featfn = featfirst[:-1]+'.dat'
-                #print "[process_one_update: log] Removing file {}".format(featfn)
-                os.remove(featfn)
-            except Exception as inst:
-                print "[finalizer: error] Could not cleanup. Error was: {}".format(inst)
-        queueOut.task_done()
+            ## Mark as done
+            # mark precomp_sim true in escorts_images_sha1_infos_dev
+            searcher_finalizer.indexer.write_batch(batch_mark_precomp_sim, searcher_finalizer.indexer.table_sha1infos_name)
+            # mark info:precomp_finish in escorts_images_updates_dev
+            if not corrupted: # do not mark finished if we faced some issue? mark as corrupted?
+                searcher_finalizer.indexer.write_batch([(update_id, {searcher_finalizer.indexer.precomp_end_marker: 'True'})],
+                                                       searcher_finalizer.indexer.table_updateinfos_name)
+            print "[finalizer: log] Finalize update {} at {} in {}s total.".format(update_id, get_now(), time.time() - start_precomp)
+            sys.stdout.flush()
+            ## Cleanup
+            if simname:
+                try:
+                    # remove simname 
+                    os.remove(simname)
+                    # remove features file
+                    featfirst = simname.split('sim')[0]
+                    featfn = featfirst[:-1]+'.dat'
+                    #print "[process_one_update: log] Removing file {}".format(featfn)
+                    os.remove(featfn)
+                except Exception as inst:
+                    print "[finalizer: error] Could not cleanup. Error was: {}".format(inst)
+            queueOut.task_done()
+        except Exception as inst:
+            print "[finalizer: error] Caught error at {}. Leaving. Error was: {}".format(get_now(), inst)
+            return end_finalizer(queueOut, queueFinalizer)
 
 
 def get_now():
@@ -271,24 +312,15 @@ def parallel_precompute(global_conf_file):
 
     # Wait for everything to be started properly
     producerOK = queueProducer.get()
-    queueProducer.task_done()
     finalizerOK = queueFinalizer.get()
-    queueFinalizer.task_done()
     for i in range(nb_workers):
         consumerOK = queueConsumer.get()
-        queueConsumer.task_done()
     print "[parallel_precompute: log] All workers are ready."
     sys.stdout.flush()
-    queueProducer.join()
-    queueFinalizer.join()
-    queueConsumer.join()
-    print "[parallel_precompute: log] Queues for workers creation joined."
     # Wait for everything to be finished
-    time.sleep(time_sleep)
-    print "[parallel_precompute: log] Join queues."
-    queueIn.join()
-    #queueOut.join()
-    print "[parallel_precompute: log] Queues joined."
+    finalizerEnded = queueFinalizer.get()
+    print finalizerEnded
+    return
     
 
 
@@ -304,6 +336,7 @@ if __name__ == "__main__":
     while True:
         parallel_precompute(global_conf_file)
         print "[precompute_similar_images_parallel: log] Nothing to compute. Sleeping for {}s.".format(time_sleep)
+        sys.stdout.flush()
         time.sleep(time_sleep)
     
     
