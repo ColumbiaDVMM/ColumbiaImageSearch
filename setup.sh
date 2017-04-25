80d2ba490d47777f6246e5ee46d87137ee9d6122
Svebor Karaman <svebor.karaman@columbia.edu>
2017-04-25 22:09:33 +0000

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# compile hashing C++ code
+echo "Compiling hashing C++ code"
+cd cu_image_search/hashing_new && make;
+
+# compile python wrapper
+echo "Compiling hashing python wrapper code"
+cd python && ./comp.sh;
+
+echo "Testing hashing python wrapper"
+python test_hashobjpy.py
+
