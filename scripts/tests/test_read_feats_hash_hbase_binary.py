8a374457b769486eb70bfcb2884f9f0bf23a038f
Svebor Karaman <svebor.karaman@columbia.edu>
2017-04-26 19:44:13 +0000

    test hbase need conf file as paramter

--- scripts/tests/test_read_feats_hash_hbase_binary.py
+++ scripts/tests/test_read_feats_hash_hbase_binary.py
@@ -6,12 +6,12 @@ import numpy as np
 
 from cu_image_search.indexer.hbase_indexer import HBaseIndexer
 from cu_image_search.memex_tools.sha1_tools import get_SHA1_from_file
-from cu_image_search.memex_tools.binary_file import read_binary_file, write_binary_file 
+from cu_image_search.memex_tools.binary_file import read_binary_file, write_binary_file
 
 if __name__=="__main__":
     HBI = HBaseIndexer('../../conf/global_var_remotehbase.json')
-    tab_sha1_name = 'escorts_images_sha1_infos_ext'    
-    
+    tab_sha1_name = 'escorts_images_sha1_infos_ext'
+
     sha1_to_check = ['0000007031E3FA80C97940017253BEAB542EA334','000000A7CAD184BD6BC00DAF032E5F7C818C39D0','000000D29139258BD3716C94A68CFF54A8A7C033']
     res,ok_ids = HBI.get_precomp_from_sha1(sha1_to_check,["sentibank","hashcode"])
     print res[0]
