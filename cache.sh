1a4553094100c96c4c87a6f366c93231b799efcb
Svebor Karaman <svebor.karaman@columbia.edu>
2017-04-26 20:04:56 +0000

    cache script

--- cache.sh
+++ cache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 killall vmtouch
-base_update_path="/home/ubuntu/memex/update/indexing"
-
-/home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/update/indexing/sha1_mapping /home/ubuntu/memex/update/indexing/hash_bits/ /home/ubuntu/memex/update/indexing/comp_features/ /home/ubuntu/memex/update/indexing/comp_idx/
+base_update_path="/home/ubuntu/memex/update/indexing/indexing-2017-04-04"
+repo_path="/home/ubuntu/memex/ColumbiaImageSearch/"
+${repo_path}/tools/vmtouch -m 400G -vld ${base_update_path}/sha1_mapping ${base_update_path}/hash_bits/ ${base_update_path}/comp_features/ ${base_update_path}/comp_idx/
 
 #/home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/
 #/home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/update/indexing/sha1_mapping /home/ubuntu/memex/update/indexing/hash_bits/ /home/ubuntu/memex/update/indexing/comp_features/ /home/ubuntu/memex/update/indexing/comp_idx/
