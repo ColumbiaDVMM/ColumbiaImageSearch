4d4d45942ea6473280b311c828b19164b5292fd4
Svebor Karaman <svebor.karaman@gmail.com>
2017-04-26 15:53:28 -0400

    adding vmtouch

--- cache.sh
+++ cache.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 killall vmtouch
-#/home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/
+base_update_path="/home/ubuntu/memex/update/indexing"
+
 /home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/update/indexing/sha1_mapping /home/ubuntu/memex/update/indexing/hash_bits/ /home/ubuntu/memex/update/indexing/comp_features/ /home/ubuntu/memex/update/indexing/comp_idx/
+
+#/home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/
+#/home/ubuntu/memex/vmtouch -m 400G -vld /home/ubuntu/memex/update/indexing/sha1_mapping /home/ubuntu/memex/update/indexing/hash_bits/ /home/ubuntu/memex/update/indexing/comp_features/ /home/ubuntu/memex/update/indexing/comp_idx/
