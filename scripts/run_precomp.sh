c5b5499cf5ba29c4d59fd753970d44a849df3801
Svebor Karaman <svebor.karaman@gmail.com>
2017-04-26 16:39:07 -0400

    run_precomp.sh for release

--- scripts/run_precomp.sh
+++ scripts/run_precomp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 start_date=$(date +%Y-%m-%d)
-conf_file="../conf/global_var_sample_precomp.json"
+#conf_file="../conf/global_var_sample_precomp.json"
+conf_file="../conf/global_var_sample_precomp_release.json"
 python precompute_similar_images_parallel.py ${conf_file}  &> log_precomp_${start_date}.txt
