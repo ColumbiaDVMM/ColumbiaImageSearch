cb10787e75ecef21417b44b007fe76aec9708516
Svebor Karaman <svebor.karaman@columbia.edu>
2017-05-30 19:07:45 +0000

    set conf file from bashrc

--- cu_image_search/www/keep_alive_api.sh
+++ cu_image_search/www/keep_alive_api.sh
@@ -1,7 +1,21 @@
 #/bin/bash
+
+# set CONF_FILE from argument if any
+#CONF_FILE=$1
+# or assume environment variable
+
+echo ${CONF_FILE}
+
 while true;
 do
-    python api.py &> logAPI$(date +%Y-%m-%d).txt;
+    if [ ${CONF_FILE+x} ]; then {
+        echo "Using conf file: "${CONF_FILE};
+        python api.py -c ${CONF_FILE} &> logAPI$(date +%Y-%m-%d).txt;
+    } else {
+       echo "Using default conf file.";
+       python api.py &> logAPI$(date +%Y-%m-%d).txt;
+    }
+    fi
     echo "["$(date)"] API crashed." >> logAPI_keep_alive.txt;
     sleep 5;
 done
