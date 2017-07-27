b0ecc3b312ad42d0e4110ffd49be72a614a40f7a
Svebor Karaman <svebor.karaman@gmail.com>
2017-07-27 17:09:27 -0400

    Retraining with finer clustering

--- www/run_face_search_api.py
+++ www/run_face_search_api.py
@@ -22,8 +22,8 @@ def after_request(response):
   return response
 
 # default conf file
-global_conf_file = '../conf/global_conf_facesearch_lopq_pretrained.json'
-#global_conf_file = '../conf/global_conf_facesearch_lopq.json'
+#global_conf_file = '../conf/global_conf_facesearch_lopq_pretrained.json'
+global_conf_file = '../conf/global_conf_facesearch_lopq.json'
 #global_conf_file = '../conf/global_conf_facesearch_faiss.json'
 
 api.add_resource(face_api_lopq.APIResponder, '/cufacesearch/<string:mode>')
