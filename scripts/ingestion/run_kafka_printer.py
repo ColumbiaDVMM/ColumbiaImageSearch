5d43105792e8a79a10d52406305b4457112939c9
Svebor Karaman <svebor.karaman@gmail.com>
2017-09-11 16:36:00 -0400

    kafka printer

---
+++ scripts/ingestion/run_kafka_printer.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+from cufacesearch.ingester.generic_kafka_processor import KafkaPrinter, default_printer_prefix
+
+if __name__ == "__main__":
+
+  # Get conf file
+  parser = ArgumentParser()
+  parser.add_argument("-c", "--conf", dest="conf_file", required=True)
+  parser.add_argument("-p", "--prefix", dest="prefix", default=default_printer_prefix)
+  options = parser.parse_args()
+
+  print options
+
+  kp = KafkaPrinter(options.conf_file, prefix=options.prefix)
+
+  # Ingest
+  for msg in kp.consumer:
+    kp.process_one(msg)
