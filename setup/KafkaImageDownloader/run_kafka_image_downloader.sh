712832a6027d5d4ad7f9211989512d2736dc690d
Svebor Karaman <svebor.karaman@columbia.edu>
2017-09-11 20:44:24 +0000

    use threaded in run image script

--- setup/KafkaImageDownloader/run_kafka_image_downloader.sh
+++ setup/KafkaImageDownloader/run_kafka_image_downloader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python ../../scripts/ingestion/run_image_ingestion.py -d -c ../../conf/global_conf_facesearch_kafka_hg_release.json &> log_image_ingestion_$(date +%Y-%m-%d).txt
+python ../../scripts/ingestion/run_image_ingestion.py -t -d -c ../../conf/global_conf_facesearch_kafka_hg_release.json &> log_image_ingestion_$(date +%Y-%m-%d).txt
 
 
