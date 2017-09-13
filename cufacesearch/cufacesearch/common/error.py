29c56930cbf5e49a53f6f1cca2cf292063c57d98
Svebor Karaman <svebor.karaman@gmail.com>
2017-09-13 00:25:43 -0400

    error

---
+++ cufacesearch/cufacesearch/common/error.py
@@ -0,0 +1,8 @@
+def full_trace_error(err_msg):
+  import sys, traceback
+  print err_msg
+  exc_type, exc_value, exc_traceback = sys.exc_info()
+  print "*** print_tb:"
+  traceback.print_tb(exc_traceback, file=sys.stdout)
+  print "*** print_exception:"
+  traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stdout)
