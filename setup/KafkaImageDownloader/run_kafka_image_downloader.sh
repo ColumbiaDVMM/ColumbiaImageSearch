4f39117d3dc51f1936f2a95322db3121f70619f2
svebor karaman <svebor.karaman@gmail.com>
2017-09-18 11:16:47 -0400

    update kafka image downloader setup

--- setup/KafkaImageDownloader/run_kafka_image_downloader.sh
+++ setup/KafkaImageDownloader/run_kafka_image_downloader.sh
@@ -3,6 +3,23 @@
 suffix="_test"
 #suffix="_release"
 
+while getopts r: option
+do
+  case "${option}"
+  in
+  r) repo_path=${OPTARG};;
+  esac
+done
+
+if [ ${repo_path+x} ]; then
+  echo "repo_path: "${repo_path}
+else
+  echo "repo_path not set. Use -r to set repo_path please."
+  exit -1
+fi
+
+cd ${repo_path}/setup/KafkaImageDownloader
+
 python ../../scripts/ingestion/run_image_ingestion.py -t -d -c ../../conf/conf_kafka_image_downloader${suffix}.json &> log_image_ingestion${suffix}_$(date +%Y-%m-%d).txt
 
 
--- setup/KafkaImageDownloader/start_docker_kafka_image_downloader.sh
+++ setup/KafkaImageDownloader/start_docker_kafka_image_downloader.sh
@@ -72,4 +72,4 @@ ${SUDO} docker rm ${docker_name}
 ## Start API
 ${SUDO} docker run -tid -v ${repo_path}:${indocker_repo_path} --cap-add IPC_LOCK --name=${docker_name} ${docker_image}:${docker_image_tag}
 echo "Starting Kafka Image Downloading"
-${SUDO} docker exec -itd ${docker_name} bash ${start_script}
+${SUDO} docker exec -itd ${docker_name} bash ${start_script}  -r ${indocker_repo_path}
