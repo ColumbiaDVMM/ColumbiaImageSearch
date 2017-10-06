c141e83cd1dbeb392f82a46f4bb87c217550e2e2
svebor karaman <svebor.karaman@gmail.com>
2017-10-06 15:27:39 -0400

    add hour minute and second to log name

--- setup/SentibankPyCaffeImageProcessing/run_sentibank_pycaffe_image_processing.sh
+++ setup/SentibankPyCaffeImageProcessing/run_sentibank_pycaffe_image_processing.sh
@@ -28,7 +28,7 @@ extr_type="sbpycaffe"
 conf="conf_extr_"${extr_type}${suffix}".json"
 # Should these two script be run on different machines?
 # extraction_checker could be run on the same machine as the search API? or as the image downloader one?
-python ../../${package_name}/${package_name}/updater/extraction_checker.py -d -c ../../conf/${conf} &> log_check${suffix}_${extr_type}_$(date +%Y-%m-%d).txt &
-python ../../${package_name}/${package_name}/updater/extraction_processor.py -c ../../conf/${conf} &> log_proc${suffix}_${extr_type}_$(date +%Y-%m-%d).txt
+python ../../${package_name}/${package_name}/updater/extraction_checker.py -d -c ../../conf/${conf} &> log_check${suffix}_${extr_type}_$(date +%Y-%m-%d_%H-%M-%S).txt &
+python ../../${package_name}/${package_name}/updater/extraction_processor.py -c ../../conf/${conf} &> log_proc${suffix}_${extr_type}_$(date +%Y-%m-%d_%H-%M-%S).txt
 
 
