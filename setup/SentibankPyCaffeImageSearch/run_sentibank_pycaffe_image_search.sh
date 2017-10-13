e239ecb989b8617e2b0eea9377a276da529b180d
svebor karaman <svebor.karaman@gmail.com>
2017-10-12 22:49:01 -0400

    extended date format for search

--- setup/SentibankPyCaffeImageSearch/run_sentibank_pycaffe_image_search.sh
+++ setup/SentibankPyCaffeImageSearch/run_sentibank_pycaffe_image_search.sh
@@ -29,5 +29,5 @@ conf="conf_search_"${extr_type}${suffix}".json"
 # extraction_checker could be run on the same machine as the search API? or as the image downloader one?
 #echo "python ../../www/run_search_api.py -c ../../conf/${conf} &> log_searchapi${suffix}_${extr_type}_$(date +%Y-%m-%d).txt" >> ~/tmp.txt
 
-python ../../www/run_search_api.py -c ../../conf/${conf} &> log_searchapi${suffix}_${extr_type}_$(date +%Y-%m-%d).txt
+python ../../www/run_search_api.py -c ../../conf/${conf} &> log_searchapi${suffix}_${extr_type}_$(date +%Y-%m-%d_%H-%M-%S).txt
 
