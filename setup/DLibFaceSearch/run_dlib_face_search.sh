e0c3b6182fd2c3d552dab06b61539564336b0206
svebor karaman <svebor.karaman@gmail.com>
2017-10-19 19:19:55 -0400

    endpoint name for face

--- setup/DLibFaceSearch/run_dlib_face_search.sh
+++ setup/DLibFaceSearch/run_dlib_face_search.sh
@@ -30,7 +30,7 @@ conf="conf_search_"${extr_type}${suffix}".json"
 # extraction_checker could be run on the same machine as the search API? or as the image downloader one?
 # Should we have a script that make sure this process are still alive akin to the keep_alive_api.sh scripts...
 
-python ../../www/api.py -c ../../conf/${conf} &> log_searchapi${suffix}_${extr_type}_$(date +%Y-%m-%d_%H-%M-%S).txt
+python ../../www/run_search_api.py -c ../../conf/${conf} -e "cufacesearch" &> log_searchapi${suffix}_${extr_type}_$(date +%Y-%m-%d_%H-%M-%S).txt
 
 
 
