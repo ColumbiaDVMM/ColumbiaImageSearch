8317763cb54b9e707dbe4a95598f473662516cad
Svebor Karaman <svebor.karaman@columbia.edu>
2017-10-25 21:38:20 +0000

    base_path openstack

--- setup/SentibankPyCaffeImageSearch/start_docker_sentibank_pycaffe_image_search.sh
+++ setup/SentibankPyCaffeImageSearch/start_docker_sentibank_pycaffe_image_search.sh
@@ -2,9 +2,9 @@
 
 ## Adjust that to the actual host values
 # TODO: set this, base_path on host machine
-base_path=/media/data/Code/MEMEX/
+#base_path=/media/data/Code/MEMEX/
 #base_path=/Users/svebor/Documents/Workspace/CodeColumbia/MEMEX/
-#base_path=~
+base_path=~
 
 # You should not need to change that,
 # and if you do, make sure that the config file reflects these changes
