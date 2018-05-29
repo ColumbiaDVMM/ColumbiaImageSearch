b00da7f6d84284bf8f47c98f659152eb07ee831f
svebor karaman <svebor.karaman@gmail.com>
2018-05-29 19:21:02 -0400

    full trace error python 3

--- cufacesearch/cufacesearch/common/error.py
+++ cufacesearch/cufacesearch/common/error.py
@@ -1,9 +1,9 @@
 def full_trace_error(err_msg):
   import sys, traceback
-  print err_msg
+  print(err_msg)
   exc_type, exc_value, exc_traceback = sys.exc_info()
-  print "*** print_tb:"
+  print("*** print_tb:")
   traceback.print_tb(exc_traceback, file=sys.stdout)
-  print "*** print_exception:"
+  print("*** print_exception:")
   traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stdout)
   sys.stdout.flush()
