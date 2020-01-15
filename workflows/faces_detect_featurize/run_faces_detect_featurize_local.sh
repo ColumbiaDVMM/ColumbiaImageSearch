7fd2faf7cc462470e87d0c0de46cf233fb467576
Svebor Karaman <svebor.karaman@gmail.com>
2020-01-15 16:51:27 -0500

    clean up workflows

--- workflows/faces_detect_featurize/run_faces_detect_featurize_local.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-../../../spark-1.6.0/bin/spark-submit \
---master local[2] \
---executor-memory 2g  --executor-cores 1  --num-executors 1 \
---jars ../packages/spark-examples_2.10-2.0.0-SNAPSHOT.jar,../packages/random-0.0.1-SNAPSHOT-shaded.jar  \
---py-files ../packages/cufacesearch.zip \
-faces_detect_featurize.py   \
-$@
-
